beff7d41b2374009b7c7a1f28862f12348fef2d1
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2020-12-10 23:13:17 +0200

    add: day 1 puzzles

---
+++ 2020/day1.puzze1.js
@@ -0,0 +1,3 @@
+const sum = 1902 + 118;
+const product = 1902 * 118;
+console.log({ 1: 1902, 2: 118, sum, answer: product });
---
+++ 2020/day1.puzze2.js
@@ -0,0 +1,3 @@
+const sum = 615 + 689 + 716;
+const product = 615 * 689 * 716;
+console.log({ 1: 615, 2: 689, 3: 716, sum, answer: product });
